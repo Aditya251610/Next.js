68721ddf9758d35932fb5a4610c7d42a14d7b6d6
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-08-08 19:26:10 +0530

    created the meals page of a food app

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,11 +1,27 @@
+import MealsGrid from '@/components/Meals/MealsGrid';
+import classes from './page.module.css'
 import Link from 'next/link';
 
 export default function Meals(){
     return(
-        <div>
-            Meals
-            <p><Link href='/meals/info-1'>Meal 1</Link></p>
-            <p><Link href='/meals/info-2'>Meal 2</Link></p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>for you</span>
+                </h1>
+                <p>
+                    Choose your favourite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share Your Favourite Recipe.
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     )
 }
--- 05-onwards-foodies-starting-project/components/Header/Header.js
+++ 05-onwards-foodies-starting-project/components/Header/Header.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import classes from './Header.module.css';
 import Logo from '@/assets/logo.png';
 import HeaderBackground from '@/components/Header/HeaderBackground';
-
+import NavLink from './NavLink';
 
 export default function Header() {
     return(
@@ -18,11 +18,11 @@ export default function Header() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href='/meals'>Browse Meals</Link>
+                            <NavLink href='/meals'>Browse Meals</NavLink>
                         </li>
 
                         <li>
-                            <Link href='/community'>Foodies Community</Link>
+                            <NavLink href='/community'>Foodies Community</NavLink>
                         </li>
                     </ul>
                 </nav>
